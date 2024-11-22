9638ed3008022a8d2907af1cfe35a5fc2ba8992d
mrakotomanana <mrakotomanana.vit@gmail.com>
2024-11-22 18:46:30 +0300

    ejs front end

--- server.js
+++ server.js
@@ -12,18 +12,29 @@ const app = express();
 const optionsCors = {
     origin: "http://localhost:3000"
 };
+
 //https://cours-info.iut-bm.univ-fcomte.fr/upload/supports/S3/web/cot%20serveur/tp10_end.pdf
 const privateKey = fs.readFileSync('./certs/certificat.key', 'utf8');
 const certificate = fs.readFileSync('./certs/certificat.crt', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(cors(optionsCors));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.locals.title = "EJS Is Fun";
+    next();
+});
+
 app.get("/", (req, res) => {
-    res.json({ message: "Bienvenue dans l'application TP 10: Jetons JWT" });
+    // res.json({ message: "Bienvenue dans l'application TP 10: Jetons JWT" });
+    // res.render('index', { title: 'Bienvenue sur une application TP 10'});
+    res.locals.title = 'Bienvenue sur une application JWT Token';
+    res.render('index');
 });
 
 const server = https.createServer(credentials, app);
